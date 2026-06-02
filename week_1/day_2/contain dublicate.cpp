31dd4a1b9fc3f8b38ca8ec4e0bedeb416ac0737f
mayanksharma359 <mayankmudgal1010@gmail.com>
2026-06-02 20:58:54 +0530

    Create contain dublicate.cpp

---
+++ week_1/day_2/contain dublicate.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+bool containsDuplicate(vector<int>& nums) {
+    unordered_set<int> s;
+
+    for (int num : nums) {
+        if (s.find(num) != s.end()) {
+            return true;
+        }
+        s.insert(num);
+    }
+
+    return false;
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3, 1};
+
+    if (containsDuplicate(nums))
+        cout << "True";
+    else
+        cout << "False";
+
+    return 0;
+}
